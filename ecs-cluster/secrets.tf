b2a48803c719ad0466fc0dc3270bf1a5b2c2aeca
hassamdevops248 <hassam.ul.haq@kitohealth.org>
2026-09-09 19:31:59 +0500

    set secret

---
+++ ecs-cluster/secrets.tf
@@ -0,0 +1,8 @@
+# Dev env values Secrets Manager se aate hain — repo mein koi secret nahi
+data "aws_secretsmanager_secret_version" "dev" {
+  secret_id = "kito_dev_api"
+}
+
+locals {
+  dev_env = jsondecode(data.aws_secretsmanager_secret_version.dev.secret_string)
+}
